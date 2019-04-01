10c15daed3a757f0c15a7a80f18bb2aee86136a5
tomgiagtz <tom.giagtzoglou@gmail.com>
2019-03-31 20:57:59 -0400

    clean up routes in RootConatiner

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import { Route, withRouter } from "react-router-dom";
-import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
 import HomeContainer from "./HomeContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import ResumeContainer from "./ResumeContainer";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faFolderOpen, faFile } from "@fortawesome/free-solid-svg-icons";
 import MenuComponent from "../components/MenuComponent";
 
 const StyledRoot = styled.div`
@@ -25,39 +22,31 @@ class RootContainer extends Component {
 		this.setState({ expanded });
 	};
 
-	
-
 	render() {
 		const { expanded } = this.state;
 		return (
-			<Route
-				render={() => {
-					return (
-						<React.Fragment>
-							<MenuComponent onToggle={this.onToggle}/>
-							<StyledRoot expanded={expanded}>
-								<Route
-									path="/"
-									exact
-									component={props => <HomeContainer />}
-								/>
-								<Route
-									path="/home"
-									component={props => <HomeContainer />}
-								/>
-								<Route
-									path="/resume"
-									component={props => <ResumeContainer />}
-								/>
-								<Route
-									path="/portfolio"
-									component={props => <PortfolioContainer />}
-								/>
-							</StyledRoot>
-						</React.Fragment>
-					);
-				}}
-			/>
+			<React.Fragment>
+				<MenuComponent onToggle={this.onToggle} />
+				<StyledRoot expanded={expanded}>
+					<Route
+						path="/"
+						exact
+						component={props => <HomeContainer />}
+					/>
+					<Route
+						path="/home"
+						component={props => <HomeContainer />}
+					/>
+					<Route
+						path="/resume"
+						component={props => <ResumeContainer />}
+					/>
+					<Route
+						path="/portfolio"
+						component={props => <PortfolioContainer />}
+					/>
+				</StyledRoot>
+			</React.Fragment>
 		);
 	}
 }
