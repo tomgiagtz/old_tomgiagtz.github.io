d6290f36b6823b71aa32ecf736a233c20db4e435
tomgiagtz <tom.giagtzoglou@gmail.com>
2020-02-25 14:51:12 -0500

    Updates

---
+++ precache-manifest.ae67e9dbf4f99229cc2bb83d6f14187d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc580cdbd49eab58e3e1",
+    "url": "/tomg/static/css/main.ea5a48d8.chunk.css"
+  },
+  {
+    "revision": "cc580cdbd49eab58e3e1",
+    "url": "/tomg/static/js/main.cc580cdb.chunk.js"
+  },
+  {
+    "revision": "c75bad2ba31eee16fb28",
+    "url": "/tomg/static/css/1.a795fdf2.chunk.css"
+  },
+  {
+    "revision": "c75bad2ba31eee16fb28",
+    "url": "/tomg/static/js/1.c75bad2b.chunk.js"
+  },
+  {
+    "revision": "bb5797435c02dc172726",
+    "url": "/tomg/static/js/runtime~main.bb579743.js"
+  },
+  {
+    "revision": "948de5b2eecb4eee5120cdf37ea24cbf",
+    "url": "/tomg/static/media/resume.948de5b2.pdf"
+  },
+  {
+    "revision": "a5e3ec93e3e25047059d9326c6793d1f",
+    "url": "/tomg/static/media/house-web.a5e3ec93.jpg"
+  },
+  {
+    "revision": "ccc64c4456a34078ee478064708c7824",
+    "url": "/tomg/static/media/profile.ccc64c44.jpg"
+  },
+  {
+    "revision": "3fcc535c17d7928d25d1e32bc7a00bd4",
+    "url": "/tomg/static/media/Bridge.3fcc535c.jpg"
+  },
+  {
+    "revision": "c1593c0b6f6529c051295772c0e9f45a",
+    "url": "/tomg/static/media/Circle.c1593c0b.jpg"
+  },
+  {
+    "revision": "174ca71fc0036906d39436bba5d11e88",
+    "url": "/tomg/static/media/Clutter.174ca71f.jpg"
+  },
+  {
+    "revision": "aa11fc639ea4cd7eec749e9f5c1489a0",
+    "url": "/tomg/static/media/DC Color 1.aa11fc63.jpg"
+  },
+  {
+    "revision": "3aca07d162365b18ca97c0abc1d28384",
+    "url": "/tomg/static/media/DC Color 2.3aca07d1.jpg"
+  },
+  {
+    "revision": "4fb62746c43d0d99ea809e80bf1a4c67",
+    "url": "/tomg/static/media/DrawnOnHand 12x9.4fb62746.jpg"
+  },
+  {
+    "revision": "6fc8fbc2ea86f1aed5e30332ec79da85",
+    "url": "/tomg/static/media/Gambino.6fc8fbc2.jpg"
+  },
+  {
+    "revision": "3cb3e406db646d399d98df9a6bc38dbb",
+    "url": "/tomg/static/media/Leaf.3cb3e406.jpg"
+  },
+  {
+    "revision": "4f66da98f08bf3391491078bb9a87878",
+    "url": "/tomg/static/media/Plane Wing 15x12.4f66da98.jpg"
+  },
+  {
+    "revision": "5aa21918cd657bb6590e7cb7cac04dd5",
+    "url": "/tomg/static/media/Study Of Fabric.5aa21918.jpg"
+  },
+  {
+    "revision": "f07d83711336f389c4f5c50d8a5a815c",
+    "url": "/tomg/static/media/Tower.f07d8371.jpg"
+  },
+  {
+    "revision": "94d8954889633cffeb918d9dcfb10397",
+    "url": "/tomg/static/media/footsteps.94d89548.jpg"
+  },
+  {
+    "revision": "3137a8b094f613136675e0399bceb7c8",
+    "url": "/tomg/index.html"
+  }
+];
